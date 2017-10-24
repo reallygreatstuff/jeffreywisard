cc9ffc0e5b603aac3081e4a47eb482e438d2d033
Unknown <jeffreywisard@gmail.com>
2017-10-23 21:06:09 -0400

    homework

--- 10_intro_jquery/Assignment/starter_code/js/index.js
+++ 10_intro_jquery/Assignment/starter_code/js/index.js
@@ -1,16 +1,34 @@
  $(document).ready(function() {
 
-
-
-
   $('form').submit(function(event) {
   	var inputField = $('input').val();
-        if (inputField == 'ny') {
-        	$('body').attr('src', '../images/nyc.jpg');
-        }
+    if (inputField === 'NYC' || 'new york' || 'nyc' || 'new york city') {
+    	$('body').css('background-image', "url('images/nyc.jpg')");
 
-        console.log(inputField);
-        event.preventDefault();
-      });
+    }
     
-    });
+    else if (inputField === 'San Francisco' || 'SF' || 'Bay Area' || 'sf') {
+    	$('body').css('background-image', "url('images/sf.jpg')");
+    
+    }
+
+    else if (inputField === 'Los Angeles' || 'LA' || 'la' || 'LAX') {
+    	$('body').css('background-image', "url('images/la.jpg')");
+    
+    }
+ 	
+ 	else if (inputField === 'Austin' || 'ATX' || 'atx') {
+    	$('body').css('background-image', "url('images/austin.jpg')");
+    
+    }
+
+    else if (inputField === 'Sydney' || 'SYD' || 'sy') {
+    	$('body').css('background-image', "url('images/sydney.jpg')");
+    
+    }
+
+    console.log(inputField);
+    event.preventDefault();
+  });
+ 
+});
