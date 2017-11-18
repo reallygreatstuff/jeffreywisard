9c0c8dde2a204257e84f1fa1390eaa29b12809f3
Unknown <jeffreywisard@gmail.com>
2017-11-18 10:16:21 -0500

    Even more edits!

--- Final Project/Website/js/index.js
+++ Final Project/Website/js/index.js
@@ -1,34 +1,10 @@
 
 $('.hamburgerMenu').click(function() {
-	$('nav').slideToggle('200');
+  $('nav').slideToggle('200');
 });
 
-//horray!
 
-
-$('button').click(function() {
-    $('.hit').text(randomEl(adjectives)+' '+randomEl(nouns));
-    selectElementContents($('.hit')[0]);
-});
-
-function randomEl(list) {
-    var i = Math.floor(Math.random() * list.length);
-    return list[i];
-}
-
-function selectElementContents(el) {
-    var range = document.createRange();
-    range.selectNodeContents(el);
-    var sel = window.getSelection();
-    sel.removeAllRanges();
-    sel.addRange(range);
-}
-
-var adjectives = ["Hot", "Dry", "Cold", "Cooked", "Important", "Whole", "Non", "Grain", "Favorite", "Only", "Eat", "Fortified", "Chief", "Infant", "Fiber", "Main", "Principal", "Bread", "Bran", "Breakfast", "Prepared", "Popular", "Coated", "Sugared", "Sugary", "Unsweetened", "Sweet", "Mixed", "Wheat", "Soggy", "Nutritious", "Refined", "Crispy",  "Crisp", "Coarse", "Warm", "Crunchy", "Dominant", "Flaked", "Delicious", "Cultivated", "Wild", "Instant", "Enriched", "Preferred", "Strained", "Puffed", "Raw", "Valuable", "Brown", "Nuts", "Thick", "Predominant", "Favorite", "Thin", "Flavored", "Boxed", "Hardy", "Healthy", "Uncooked", "Enough", "Presweetened", "Based", "Legume", "Shaped"];
-var nouns = ["Flakes","Holes", "O's", "Puffs", "Crisp", "Smacks", "Squares", "Charms", "Bran", "Delight", "Bits", "Berries", "Crunch", "Toasters", "Balls", "Nut", "Bites", "Munch", "Grahams", "Oats", "Bits"];
-
-$('button').click();
-
+//Slider
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -50,8 +26,9 @@ function showDivs(n) {
      x[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" w3-white", "");
+     dots[i].className = dots[i].className.replace(" w3-red", "");
   }
   x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+  dots[slideIndex-1].className += " w3-red";
 }
+
