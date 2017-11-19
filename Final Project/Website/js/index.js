3c18273b1025e9a07589062c970c2be664dc6654
Wylesha Rachell <wfield1@gmail.com>
2017-11-19 15:33:04 -0500

    Updates to fix functionality

--- Final Project/Website/js/index.js
+++ Final Project/Website/js/index.js
@@ -15,9 +15,12 @@ $(document).ready(function(){
       }
     });
 
-    var slideIndex = 1;
-    showDivs(slideIndex);
 
+    if ($('.slider').length) {
+      var slideIndex = 1;
+      showDivs(slideIndex);
+    }
+    
     function plusDivs(n) {
       showDivs(slideIndex += n);
     }
@@ -43,7 +46,7 @@ $(document).ready(function(){
     }
 
     //Cereal Generator
-    $('.cerealGeneator button').click(function() {
+    $('.cerealGenerator button').click(function() {
         $('.hit').text(randomEl(adjectives)+' '+randomEl(nouns));
         selectElementContents($('.hit')[0]);
     });
@@ -64,5 +67,5 @@ $(document).ready(function(){
     var adjectives = ["Hot", "Dry", "Cold", "Cooked", "Important", "Whole", "Non", "Grain", "Favorite", "Only", "Eat", "Fortified", "Chief", "Infant", "Fiber", "Main", "Principal", "Bread", "Bran", "Breakfast", "Prepared", "Popular", "Coated", "Sugared", "Sugary", "Unsweetened", "Sweet", "Mixed", "Wheat", "Soggy", "Nutritious", "Refined", "Crispy",  "Crisp", "Coarse", "Warm", "Crunchy", "Dominant", "Flaked", "Delicious", "Cultivated", "Wild", "Instant", "Enriched", "Preferred", "Strained", "Puffed", "Raw", "Valuable", "Brown", "Nuts", "Thick", "Predominant", "Favorite", "Thin", "Flavored", "Boxed", "Hardy", "Healthy", "Uncooked", "Enough", "Presweetened", "Based", "Legume", "Shaped"];
     var nouns = ["Flakes","Holes", "O's", "Puffs", "Crisp", "Smacks", "Squares", "Charms", "Bran", "Delight", "Bits", "Berries", "Crunch", "Toasters", "Balls", "Nut", "Bites", "Munch", "Grahams", "Oats", "Bits"];
 
-    $('.cerealGeneator button').click();
+    $('.cerealGenerator button').click();
 })
